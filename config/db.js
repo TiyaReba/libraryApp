059919a7b5c17c8941ffe16b1b1feda994e20010
Tiya <tiyars@gmail.comgit config --global user.name Tiyagit branch -M main git config --global user.email tiyars@gmail.com>
2022-08-10 14:24:44 +0530

    db run

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const connectDB = async () =>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+// const connectDB = async () =>{
+//     try{
+//         const conn = await mongoose.connect(process.env.MONGO_URI)
 
-        console.log(`MongoDB Connected:${conn.connection.host}`.cyan.underline);
-    }
-    catch(error){
-        console.log(error);
-        process.exit(1)
-    }
-}
+//         console.log(`MongoDB Connected:${conn.connection.host}`.cyan.underline);
+//     }
+//     catch(error){
+//         console.log(error);
+//         process.exit(1)
+//     }
+// }
 
 
-// mongoose.connect(`mongodb+srv://tiya:@cluster0.qh8z9se.mongodb.net/?retryWrites=true&w=majority`)
+// // mongoose.connect(`mongodb+srv://tiya:@cluster0.qh8z9se.mongodb.net/?retryWrites=true&w=majority`)
 
-module.exports = connectDB
+// module.exports = connectDB
--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+mongoose.connect('mongodb+srv://tiya:tiya@cluster0.qh8z9se.mongodb.net/?retryWrites=true&w=majority');
 const Schema = mongoose.Schema;
 
 
