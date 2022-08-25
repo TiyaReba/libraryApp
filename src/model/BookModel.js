1bb3b616ffaa641bf739b6b96a8a625081f1ccf2
Tiya <tiyars@gmail.comgit config --global user.name Tiyagit branch -M main git config --global user.email tiyars@gmail.com>
2022-08-25 22:08:10 +0530

    password change

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb+srv://tiya:tiya@cluster0.qh8z9se.mongodb.net/?retryWrites=true&w=majority');
 // mongoose.connect('mongodb://localhost:27017/Library');
 
-mongoose.connect('mongodb+srv://tiya:1DsDl6uWqKVpVheH@authordata.q6ugz9d.mongodb.net/casestudy?retryWrites=true&w=majority');
+mongoose.connect("mongodb+srv://tiya:DxkNH6vF0qD0L2ud@authordata.q6ugz9d.mongodb.net/casestudy",{ useNewUrlParser: true, useUnifiedTopology: true });
 const Schema = mongoose.Schema;
 
 
